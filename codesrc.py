45804101b224f723459b5abb3e27cd044ace1028
arthur <arthur.gaudebert@gmail.com>
2023-04-24 19:24:41 +0200

    deplacement en super

--- codesrc.py
+++ codesrc.py
@@ -1,3 +1,7 @@
+class chess:
+    def __init__(self,tab):
+
+
 class piece:
     def __init__(self,couleur,x,y,nb_coup):
         self.__couleur=couleur          #0 pour noir, 1 pour blanc
@@ -25,6 +29,14 @@ class piece:
 
     def print_coordonne(self):
         print("x = ",self.get_x(), " y = ",self.get_y())
+
+    def deplacement(self,dx,dy):
+        if ((self.__x+dx>7)or(self.__y+dy>7)):
+            print("Erreur: deplacement trop important")
+            return False
+        else:
+            self.set_nb_coup(self.get_nb_coup()+1)
+            return True
     
     
 
@@ -34,16 +46,20 @@ class pion(piece):
         if((piece.get_nb_coup(self)==0 and dy>2)or(piece.get_nb_coup(self)>0 and dy>1)):
             print("Erreur : deplacement non autorise")
         else:
-            piece.set_y(self,piece.get_y(self)+dy)
-            piece.set_nb_coup(self,piece.get_nb_coup(self)+1)
+            if(super().deplacement(0,dy)):
+                piece.set_y(self,piece.get_y(self)+dy)
+                piece.set_nb_coup(self,piece.get_nb_coup(self)+1)
     
+
 
 pion1=pion(1,0,1,0)
 pion1.print_coordonne()
 pion1.deplacement(2)
 pion1.print_coordonne()
-pion1.deplacement(2)
-pion1.print_coordonne()
+
+tab=((5,5),(6,6),(7,7))
+print(tab[0][1])
+
 
 
 
